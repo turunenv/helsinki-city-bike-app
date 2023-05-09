cc2f810e43fc2635b032b62bfa36f9bd119a582e
Verneri Turunen <turunenv@outlook.com>
2023-05-09 14:26:19 +0300

    handle errors, log requests

--- app/server.js
+++ app/server.js
@@ -3,6 +3,11 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, sep } from 'path';
 
+import { stationRouter } from './api/routes/stations.js';
+
+import { errorHandler, requestLogger } from './middleware.js';
+import { error } from 'console';
+
 //configuration
 const __dirname = dirname(fileURLToPath(import.meta.url)) + sep;
 const config = {
@@ -10,6 +15,7 @@ const config = {
   dir: {
     root: __dirname,
     public: __dirname + 'static' + sep,
+    views: __dirname + 'views' + sep,
   },
 };
 
@@ -17,12 +23,28 @@ const app = express();
 
 app.use(express.static(config.dir.public));
 
+app.set('view engine', 'ejs');
+app.set('views', config.dir.views);
+
+//log requests
+app.use(requestLogger);
+
 //base route
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
+  res.render('index');
 });
+
+app.use('/stations', stationRouter);
+
+//handle requests to non-existent paths
+app.use((req, res) => {
+  const title = 'This page does not exist';
+  res.render('error', { title, message: req.url });
+});
+
+//handle other errors
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.log(`app running on port ${config.port}`);
 });
-
