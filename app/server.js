c3f1ad69a7fc6f719531c556ef20afac89fbaaa9
Verneri Turunen <turunenv@outlook.com>
2023-05-09 09:58:13 +0300

    skeleton app with initial config up and running

--- app/server.js
+++ app/server.js
@@ -1,3 +1,28 @@
 import express from 'express';
 
-const app = express;
+import { fileURLToPath } from 'url';
+import { dirname, sep } from 'path';
+
+//configuration
+const __dirname = dirname(fileURLToPath(import.meta.url)) + sep;
+const config = {
+  port: process.env.PORT || 3000,
+  dir: {
+    root: __dirname,
+    public: __dirname + 'static' + sep,
+  },
+};
+
+const app = express();
+
+app.use(express.static(config.dir.public));
+
+//base route
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
+app.listen(config.port, () => {
+  console.log(`app running on port ${config.port}`);
+});
+
