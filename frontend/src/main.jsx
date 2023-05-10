2eba6c001fbd801698c51009c79a6babc87011e6
Verneri Turunen <turunenv@outlook.com>
2023-05-10 14:22:07 +0300

    remove boilerplate, add react router and first routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+import ErrorPage from './error-page';
+import Root from './routes/root';
+import Stations from './routes/stations';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/stations',
+    element: <Stations />,
+    errorElement: <ErrorPage />
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
