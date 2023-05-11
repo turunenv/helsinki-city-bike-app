d4e961fcea184365bea359035ab7d7836fd6c80d
Verneri Turunen <turunenv@outlook.com>
2023-05-11 19:58:48 +0300

    add name filter to stations view

--- frontend/src/components/JourneyFilter.jsx
+++ frontend/src/components/JourneyFilter.jsx
@@ -24,34 +24,73 @@ export default function JourneyFilter({
 
   return (
     <div className='journey-filter-container'>
-      <fieldset onChange={handleOrderByChange} id="journey-order-criterion">
+      <fieldset id="journey-order-criterion">
         <legend>Order By:</legend>
         <div>
-          <input type="radio" id="no-order" name="no-order" value="no-order" checked={orderBy === "no-order"}/>
+          <input 
+          type="radio" 
+          id="no-order" 
+          name="no-order" 
+          value="no-order" 
+          checked={orderBy === "no-order"}
+          onChange={handleOrderByChange} 
+          />
           <label htmlFor="no-order">No specific order</label>
-          </div>
+        </div>
         <div>
-          <input type="radio" id="departure" name="departure" value="departure" checked={orderBy === "departure"}/>
-          <label htmlFor="">Departure time</label>
-          </div>
+          <input 
+          type="radio" 
+          id="departure" 
+          name="departure" 
+          value="departure" 
+          checked={orderBy === "departure"}
+          onChange={handleOrderByChange} 
+          />
+          <label htmlFor="departure">Departure time</label>
+        </div>
         <div>
-          <input type="radio" id="dist" name="dist" value="dist" checked={orderBy === "dist"}/>
-          <label htmlFor="">Distance covered</label>
-          </div>
+          <input 
+          type="radio" 
+          id="dist" 
+          name="dist" 
+          value="dist" 
+          checked={orderBy === "dist"}
+          onChange={handleOrderByChange} 
+          />
+          <label htmlFor="dist">Distance covered</label>
+        </div>
         <div>
-          <input type="radio" id="duration" name="duration" value="duration" checked={orderBy === "duration"}/>
-          <label htmlFor="">Journey duration</label>
+          <input 
+          type="radio" 
+          id="duration" 
+          name="duration" 
+          value="duration" 
+          checked={orderBy === "duration"}
+          onChange={handleOrderByChange} 
+          />
+          <label htmlFor="duration">Journey duration</label>
         </div>
       </fieldset>
       <fieldset 
-        onChange={handleOrderDescendingChange} 
         id="journey-order-ascending"
         disabled={orderBy === 'no-order'}
         >
-        <input type="radio" value="order-ascending" id="order-ascending" checked={!orderByDesc} />
+        <input 
+          type="radio" 
+          value="order-ascending" 
+          id="order-ascending" 
+          checked={!orderByDesc} 
+          onChange={handleOrderDescendingChange} 
+        />
         <label htmlFor="order-ascending">Ascending</label>
 
-        <input type="radio" value="order-descending" id="order-descending" checked={orderByDesc} />
+        <input 
+          type="radio" 
+          value="order-descending" 
+          id="order-descending" 
+          checked={orderByDesc} 
+          onChange={handleOrderDescendingChange} 
+          />
         <label htmlFor="order-descending">Descending</label>
       </fieldset>
       
--- frontend/src/routes/stations.jsx
+++ frontend/src/routes/stations.jsx
@@ -12,40 +12,63 @@ export async function loader() {
 
 export default function Stations() {
   const [stationPage, setStationPage] = useState(0);
+  const [stationFilter, setStationFilter] = useState('');
 
   const { stations } = useLoaderData();
+
+  const handleFilterChange = (e) => {
+    setStationFilter(e.target.value);
+    setStationPage(0);
+  }
   
   const maxStationsPerPage = 40;
 
+  
+
+  let selectedStations = stations;
+  if (stationFilter) {
+    selectedStations = stations.filter(station => {
+      return station.nameFi.toLowerCase().includes(stationFilter)
+    })
+  } 
+
   //calculate indeces to show based on stationPage
   let startIndex = stationPage * maxStationsPerPage;
-  let stopIndex = Math.min(startIndex + maxStationsPerPage, stations.length);
+  let stopIndex = Math.min(startIndex + maxStationsPerPage, selectedStations.length);
 
-  let stationsToRender = stations.slice(startIndex, stopIndex);
-  let isLastPage = startIndex >= stations.length - maxStationsPerPage;
+  let stationsToRender = selectedStations.slice(startIndex, stopIndex);
+  let isLastPage = startIndex >= selectedStations.length - maxStationsPerPage;
 
   
   return (
     <>
       <h1>Bike Stations</h1>
-        <div className="data-container">
-          <ul>
-            {stationsToRender.map(station=> {
-              return (
-                <li key={station.stationId}>
-                  <Link to={`/stations/${station.stationId}`}>
-                    {station.nameFi}
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-        <PaginationControls 
-          page={stationPage}
-          setPage={setStationPage}
-          isLastPage={isLastPage}
-        />
+      <label>Filter by name:
+        <input 
+          type="text" 
+          value={stationFilter}
+          onChange={handleFilterChange}
+        >
+        </input>
+      </label>
+      <div className="data-container">
+        <ul>
+          {stationsToRender.map(station=> {
+            return (
+              <li key={station.stationId}>
+                <Link to={`/stations/${station.stationId}`}>
+                  {station.nameFi}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <PaginationControls 
+        page={stationPage}
+        setPage={setStationPage}
+        isLastPage={isLastPage}
+      />
     </>
   )
 }
