334447c9c9e925d619b562d4ab51189b29139472
Verneri Turunen <turunenv@outlook.com>
2023-05-15 13:16:43 +0300

    conditionally disable logging when in test mode

--- backend/database/database.js
+++ backend/database/database.js
@@ -6,10 +6,16 @@ dotenv.config();
 const DB_USER = process.env.DB_USER;
 const DB_PASSWD = process.env.DB_PASSWORD;
 
-const sequelize = new Sequelize('bikejourneydb', DB_USER, DB_PASSWD, {
+const db_config = {
   host: 'localhost',
   dialect: 'mysql',
-  logging: process.env.NODE_ENV !== 'TEST', 
-});
+};
+
+//sequelize db config object's 'logging'-option can only be false or a function
+if (process.env.NODE_ENV === 'TEST') {
+  db_config.logging = false;
+}
+
+const sequelize = new Sequelize('bikejourneydb', DB_USER, DB_PASSWD, db_config);
 
 export default sequelize;
