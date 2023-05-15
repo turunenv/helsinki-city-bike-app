2443ff53306467afd1ae8ca0c60dab69cf58dc4a
Verneri Turunen <turunenv@outlook.com>
2023-05-15 10:03:37 +0300

    add folder for testing the api, implement some tests for the /api/journeys endpoint

---
+++ backend/api/test/journeys.test.js
@@ -0,0 +1,62 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../server.js';
+import { stationRouter } from '../routes/stations.js';
+let should = chai.should();
+let assert = chai.assert;
+
+chai.use(chaiHttp);
+
+const server = chai.request(app).keepOpen();
+
+describe('Journeys', () => {
+  describe('GET /api/journeys', () => {
+    it('should return 1000 journeys', (done) => {
+      server.get('/api/journeys').end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        res.body.length.should.be.eql(1000);
+        done();
+      });
+    });
+  });
+
+  describe('GET /api/journeys?orderby=duration', () => {
+    it('should return 1000 journeys, ordered by ascending duration', (done) => {
+      server.get('/api/journeys?orderby=duration').end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        res.body.length.should.be.eql(1000);
+        for (let i = 0; i < res.body.length - 1; i++) {
+          assert.isAtMost(res.body[i].duration, res.body[i + 1].duration);
+        }
+        done();
+      });
+    });
+  });
+
+  describe('GET /api/journeys?orderby=duration&desc=true', () => {
+    it('should return 1000 journeys, ordered by descending duration', (done) => {
+      server.get('/api/journeys?orderby=duration&desc=true').end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        res.body.length.should.be.eql(1000);
+        for (let i = 0; i < res.body.length - 1; i++) {
+          assert.isAtLeast(res.body[i].duration, res.body[i + 1].duration);
+        }
+        done();
+      });
+    });
+  });
+
+  describe('GET /api/journeys', () => {
+    it('should GET 1000 journeys', (done) => {
+      server.get('/api/journeys').end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        res.body.length.should.be.eql(1000);
+        done();
+      });
+    });
+  });
+});
