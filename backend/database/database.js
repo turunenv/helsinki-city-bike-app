2a282c06c1cdef114768cd683110f1665c000072
Verneri Turunen <turunenv@outlook.com>
2023-05-15 10:07:21 +0300

    disable sequelize logging if in test-mode

--- backend/database/database.js
+++ backend/database/database.js
@@ -9,6 +9,7 @@ const DB_PASSWD = process.env.DB_PASSWORD;
 const sequelize = new Sequelize('bikejourneydb', DB_USER, DB_PASSWD, {
   host: 'localhost',
   dialect: 'mysql',
+  logging: process.env.NODE_ENV !== 'TEST', 
 });
 
 export default sequelize;
